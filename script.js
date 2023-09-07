15a601dc41a4f0ec9ab94d0db438beb49bdb36bd
thearchitect222 <137104078+thearchitect222@users.noreply.github.com>
2023-09-06 23:37:48 -0400

    Co-authored-by: toyosi732 <toyosi732@users.noreply.github.com>  and Deb

--- script.js
+++ script.js
@@ -3,9 +3,5 @@ const donationSubmitBtn = document.getElementById("donation-submit-btn");
 donationSubmitBtn.addEventListener("click", (event) => {
     event.preventDefault()
     console.log('myEvent');
-//   if (email.validity.typeMismatch) {
-//     email.setCustomValidity("I am expecting an email address!");
-//   } else {
-//     email.setCustomValidity("");
-//   }
 });
+
